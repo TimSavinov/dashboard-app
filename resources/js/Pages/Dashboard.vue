1467f825f89d2e222f59f08246053abbd67cee88
TimSavinov <savinov.tim@gmail.com>
2021-07-20 16:58:19 +0300

    project initialization, authentication done

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,33 @@
+<template>
+    <breeze-authenticated-layout>
+        <template #header>
+            <div class="flex justify-between">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    Dashboard
+                </h2>
+                <breeze-button>
+                    Settings
+                </breeze-button>
+            </div>
+        </template>
+
+        <template #dashboard-content>
+           <div>
+               {{ $page.props }}
+           </div>
+        </template>
+    </breeze-authenticated-layout>
+</template>
+
+<script>
+    import BreezeAuthenticatedLayout from '@/Layouts/Authenticated'
+    import BreezeButton from '@/Components/Button'
+
+
+    export default {
+        components: {
+            BreezeAuthenticatedLayout,
+            BreezeButton,
+        },
+    }
+</script>
